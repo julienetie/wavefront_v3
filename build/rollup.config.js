8c44f4e40a33df6233a81f6a949f57ee6b9d6c83
Julien Etienne <j8@protonmail.ch>
2018-09-01 15:27:38 +0100

    Removed jekyllhyde inital project

---
+++ build/rollup.config.js
@@ -0,0 +1,22 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import buble from 'rollup-plugin-buble';
+
+export default {
+  input: "src/wavefront.js",
+  plugins: [
+    nodeResolve({
+      jsnext: true,
+      browser: true
+    }),
+    commonjs(),
+        buble({
+			target: { chrome: 51, firefox: 48, safari: 9, edge: 14 }
+        }),
+  ],
+  output:{
+    name: "wavefront",
+    file: "dist/wavefront.js",
+    format: 'iife'
+  }
+}
--- src/wavefront.js
+++ src/wavefront.js
@@ -1,3 +1,19 @@
+const name = 'julien';
+const age = 3223;
+
+const message = `
+  hello ${name}!
+  the answer is ${40 + 2}`.toUpperCase();
+
+const person = {
+  name,
+  age,
+  message,
+  sayHello () {
+    console.log( 'hello! my name is ' + this.name );
+  }
+};
 
 
-console.log('hello world')
+const greeting = person.sayHello();
+console.log('hello world', greeting)
